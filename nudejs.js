0dc8e6405398a447ea25971e070f61e586f8b41d
Li June Choi <lijune.choi@gmail.com>
2021-02-08 04:26:28 +0900

    Initial commit

--- nudejs.js
+++ nudejs.js
@@ -8,4 +8,39 @@ $("#bar1").click(function(){
         $("#bar2").hide();
   });
 
+$("#strawberrydraggable").draggable();
+$("#crumbs").draggable();
+
+$("#strawberrydraggable").click(function(){
+        $("#strawberrydraggable").hide();
+        $("#crumbs").show();
+        });
+
+$("#nudedraggable").draggable();
+$("#crumbs").draggable();
+
+$("#nudedraggable").click(function(){
+        $("#nudedraggable").hide();
+        $("#crumbs").show();
+        });
+
+$("#oreodraggable").draggable();
+$("#crumbs").draggable();
+
+$("#oreodraggable").click(function(){
+        $("#oreodraggable").hide();
+        $("#crumbs").show();
+        });
+
+$("#earphones").hover(function(){
+         $("#daydream").css("color", "pink");
+         });
+
+$("#tired").hover(function(){
+          $("#straight").css("color", "brown");
+          });
+
+$("#blackandwhite").hover(function(){
+          $("#remote").css("color", "black");
+          });
   })
