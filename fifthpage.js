2d39ed892617d453d879639b3e2b25aa96c0b345
Li June Choi <lijune.choi@gmail.com>
2021-02-07 23:36:17 +0900

    Initial commit

--- fifthpage.js
+++
@@ -1,3 +0,0 @@
-$("#SaulBass").hover(function(){
-    $("#SaulBassinfo").css("color", "red");
- });
---
+++ strawberryjs.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+$("#bar1").click(function(){
+    $("#bar2").show();
+  });
+
+  $("#bar1").dblclick(function(){
+        $("#bar2").hide();
+  });
+
+  })
