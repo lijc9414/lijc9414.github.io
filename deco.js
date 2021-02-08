003faa8f24a6bed6cebe965e18a0a50896b83bdd
Li June Choi <lijune.choi@gmail.com>
2021-02-09 07:45:28 +0900

    Initial commit

---
+++ deco.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+$("#oreosprinkle").draggable();
+
+$("#sprinkle").draggable();
+
+$("#sprinklebrown").draggable();
+
+$("#almonds").draggable();
+
+$("#coverstraw").draggable();
+
+$("#coverchoco").draggable();
+
+$("#coveroreo").draggable();
+
+$("#covermatcha").draggable();
+
+  })
